12b588b9b3aa38a1f24c19c94de08e7e6763fb36
Ashfak Uddin <ashmanpapet@gmail.com>
2022-02-19 09:47:48 -0500

    so lost

--- recursive-search/main.cpp
+++ recursive-search/main.cpp
@@ -4,10 +4,9 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-void printBoard(int a[4][4], int r, int c)
+void printBoard(int a[4][4])
 {
 	printf("\033[2J\033[1;1H");
-	cout << "row " << r << ", col " << c << endl;
 	for(int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
@@ -30,103 +29,66 @@ void solve(int setup[4][4], int solution[4][4], bool& check, int row, int col)
 {
 	if (checkLayout(setup, solution))
 	{
+		cout << "HUH" << endl;
 		check = true;
 		return;
 	}
-	if (col == 0)
+	if (row < 3)
 	{
-		if (row < 3)
+		int i = setup[row+1][col] - 1, r = i/4, c = i%4;
+		if (r <= row || c != col)
 		{
-			setup[row][col] = setup[row + 1][col];
-			setup[row + 1][col] = 0;
-			printBoard(setup, row + 1, 1);
-	        	usleep(80000);
-			solve(setup, solution, check, row + 1, col);
-		}
-		else
-		{
-			setup[row][col] = setup[row][col + 1];
-                        setup[row][col + 1] = 0;
-                        printBoard(setup, row, col + 1);
+			setup[row][col] = setup[row+1][col];
+			setup[row+1][col] = 0;
+			printBoard(setup);
                         usleep(80000);
-                        solve(setup, solution, check, row, col + 1);
+			solve(setup, solution, check, row+1, col);
 		}
-
 	}
-	else if (row == 0)
+	if (col < 3)
 	{
-		if (col < 3)
+		int i = setup[row][col + 1] - 1, r = i/4, c = i%4;
+		if (col >= c || row != r)
 		{
-			setup[row][col] = setup[row][col + 1];
+			setup[row][col] = setup[row][col+1];
 			setup[row][col + 1] = 0;
-			printBoard(setup, row, col + 1);
-        		usleep(80000);
-			solve(setup, solution, check, row, col + 1);
-		}
-		else
-		{
-			setup[row][col] = setup[row + 1][col];
-                        setup[row + 1][col] = 0;
-                        printBoard(setup, row + 1, col);
+			printBoard(setup);
                         usleep(80000);
-                        solve(setup, solution, check, row + 1, col);
+			solve(setup, solution, check, row, col+1);
 		}
 	}
-	else if (col == 3)
+	if (row > 0)
+        {
+              int i = setup[row-1][col] - 1, r = i/4, c = i%4;
+              if (r > row)
+              {
+                        setup[row][col] = setup[row-1][col];
+                        setup[row-1][col] = 0;
+                        printBoard(setup);
+                        usleep(80000);
+                        solve(setup, solution, check, row-1, col);
+              }
+        }
+	if (col > 0)
 	{
-		if (row < 3)
+		int i = setup[row][col-1] - 1, r = i/4, c = i%4;
+		if (col < c)
 		{
 			setup[row][col] = setup[row][col-1];
-                	setup[row][col-1] = 0;
-			printBoard(setup, row, col-1);
-                	usleep(80000);
+			setup[row][col-1] = 0;
+			printBoard(setup);
+			usleep(80000);
 			solve(setup, solution, check, row, col-1);
 		}
-		else
-		{
-		 	setup[row][col] = setup[row-1][col];
-                        setup[row-1][col] = 0;
-                        printBoard(setup, row-1, col);
-                        usleep(80000);
-                        solve(setup, solution, check, row-1, col);
-		}
-	}
-	else if (row == 3)
-	{
-		if (col < 3)
-		{
-
-			setup[row][col] = setup[row][col+1];
-	                setup[row][col+1] = 0;
-	        	printBoard(setup, row, col+1);
-	        	usleep(80000);
-		        solve(setup, solution, check, row, col+1);
-		}
-		else
-		{
-			setup[row][col] = setup[row-1][col];
-                        setup[row-1][col] = 0;
-                        printBoard(setup, row-1, col);
-                        usleep(80000);
-                        solve(setup, solution, check, row-1, col);
-		}
-	}
-	else if (!check)
-	{
-		setup[row][col] = setup[row][col + 1];
-                setup[row][col + 1] = 0;
-		printBoard(setup, row, col + 1);
-        	usleep(80000);
-		solve(setup, solution, check, row, col + 1);
 	}
 }
 int main()
 {
 	int arr[4][4] =
 	{
-		{4, 1, 3, 14},
-		{5, 6, 7, 8},
-		{9, 0, 11, 13},
+		{0, 1, 3, 14},
+		{5, 8, 6, 7},
+		{9, 4, 11, 13},
 		{2, 10, 15, 12}
 	};
 	int solu[4][4] =
@@ -138,8 +100,9 @@ int main()
 	};
 	std::cout << ";\n";
 	bool check = false;
-	printBoard(arr, 2, 1);
-	solve(arr, solu, check, 2, 1);
+	printBoard(arr);
+	cout << 0 << " " << 0 << endl;
+	solve(arr, solu, check, 0, 0);
 	std::cout << "Done!\n";
 	return 0;
 }
