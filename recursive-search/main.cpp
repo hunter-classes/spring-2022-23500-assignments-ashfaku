db73c85472fc135ec21ffc9445bcb074dda5ce21
Ashfak Uddin <ashmanpapet@gmail.com>
2022-02-20 07:27:53 -0500

    well no more 0

--- recursive-search/main.cpp
+++ recursive-search/main.cpp
@@ -1,10 +1,12 @@
 #include <fstream>
 #include <string>
+#include <vector>
 #include <unistd.h>
 #include <iostream>
 using std::cout;
 using std::endl;
-void printBoard(int a[4][4])
+using std::vector;
+void printBoard(vector<vector<int>> a)
 {
 	printf("\033[2J\033[1;1H");
 	for(int i = 0; i < 4; i++)
@@ -15,7 +17,7 @@ void printBoard(int a[4][4])
 	}
 	cout << "------------------------------------------" << endl;
 }
-bool checkLayout(int a[4][4], int b[4][4])
+bool checkLayout(vector<vector<int>> a, int b[4][4])
 {
 	for (int i = 0; i < 4; i++)
 	{
@@ -25,7 +27,7 @@ bool checkLayout(int a[4][4], int b[4][4])
 	}
 	return true;
 }
-void solve(int setup[4][4], int solution[4][4], bool& check, int& row, int& col, int& a, int& b)
+void solve(vector<vector<int>> setup, int solution[4][4], bool& check, int& row, int& col, int a, int b)
 {
 	if (checkLayout(setup, solution))
 	{
@@ -39,26 +41,41 @@ void solve(int setup[4][4], int solution[4][4], bool& check, int& row, int& col,
 	row = a;
 	col = b;
 	printBoard(setup);
-	cout << a << " " << b << endl;
+	cout << row << " " << col << endl;
 	usleep(80000);
 	if (!check)
-		solve(setup, solution, check, row, col, row, --col);
+		solve(setup, solution, check, row, col, row, col - 1);
 	if (!check)
-		solve(setup, solution, check, row, col, --row, col);
+		solve(setup, solution, check, row, col, row - 1, col);
 	if (!check)
-		solve(setup, solution, check, row, col, ++row, col);
+		solve(setup, solution, check, row, col, row + 1, col);
 	if (!check)
-		solve(setup, solution, check, row, col, row, ++col);
+		solve(setup, solution, check, row, col, row, col + 1);
 }
 int main()
 {
-	int arr[4][4] =
-	{
-		{13, 2, 10, 3},
-		{1, 12, 8, 4},
-		{5, 0, 9, 6},
-		{15, 14, 11, 7}
-	};
+	vector<vector<int>> arr;
+	vector<int> a, b, e, d;
+	a.push_back(13);
+	a.push_back(2);
+	a.push_back(10);
+	a.push_back(3);
+	b.push_back(1);
+	b.push_back(12);
+	b.push_back(8);
+	b.push_back(4);
+	e.push_back(5);
+	e.push_back(0);
+	e.push_back(9);
+	e.push_back(6);
+	d.push_back(15);
+	d.push_back(14);
+	d.push_back(11);
+	d.push_back(7);
+	arr.push_back(a);
+	arr.push_back(b);
+	arr.push_back(e);
+	arr.push_back(d);
 	int solu[4][4] =
 	{
 		{1, 2, 3, 4},
@@ -75,4 +92,5 @@ int main()
 	std::cout << "Done!\n";
 	return 0;
 }
+
 
